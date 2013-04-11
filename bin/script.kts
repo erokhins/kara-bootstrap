349e52280c59a0accd8dde14efe9343254d13af2
Erokhin Stanislav <erokhins239@gmail.com>
2013-04-11 17:27:42 +0400

    new module Sample & fix run configurations

--- bin/script.kts
+++ bin/script.kts
@@ -1,12 +1,9 @@
 // Module script for production
 import kotlin.modules.*
 fun project() {
-    module("kara.bootstrap") {
-        sources += "/home/erokhins/IdeaProjects/kara-bootstrap/src/kara/bootstrap/Application.kt"
-        sources += "/home/erokhins/IdeaProjects/kara-bootstrap/src/kara/bootstrap/styles/DefaultStyles.kt"
-        sources += "/home/erokhins/IdeaProjects/kara-bootstrap/src/kara/bootstrap/views/DefaultLayout.kt"
-        sources += "/home/erokhins/IdeaProjects/kara-bootstrap/src/kara/bootstrap/routes/Home.kt"
-        sources += "/home/erokhins/IdeaProjects/kara-bootstrap/src/kara/bootstrap/views/home/IndexView.kt"
+    module("BootstrapLib") {
+        sources += "/home/erokhins/IdeaProjects/kara-bootstrap/BootstrapLib/src/kara/bootstrap/styles/BootstrapStyleClasses.kt"
+        sources += "/home/erokhins/IdeaProjects/kara-bootstrap/BootstrapLib/src/kara/bootstrap/views/BootstrapLayout.kt"
         // Classpath
         classpath += "/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/jsse.jar"
         classpath += "/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/rt.jar"
@@ -25,11 +22,11 @@ fun project() {
         classpath += "/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/ext/zipfs.jar"
         classpath += "/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/ext/pulse-java.jar"
         // Output directory, commented out
-        //         classpath += "/home/erokhins/IdeaProjects/kara-bootstrap/bin"
+        //         classpath += "/home/erokhins/IdeaProjects/kara-bootstrap/out/production/BootstrapLib"
         classpath += "/home/erokhins/IdeaProjects/kara/out/artifacts/kara_dist/lib/kotlin-runtime.jar"
         classpath += "/home/erokhins/IdeaProjects/kara/out/artifacts/kara_dist/modules/core/kara-core.jar"
         // Java Source Roots
-        classpath += "/home/erokhins/IdeaProjects/kara-bootstrap/src"
+        classpath += "/home/erokhins/IdeaProjects/kara-bootstrap/BootstrapLib/src"
         // External annotations
         annotationsPath += "/home/erokhins/scripts/idea-IU-129.178/lib/jdkAnnotations.jar"
         annotationsPath += "/home/erokhins/.IntelliJIdea12/config/plugins/Kotlin/kotlinc/lib/kotlin-jdk-annotations.jar"
